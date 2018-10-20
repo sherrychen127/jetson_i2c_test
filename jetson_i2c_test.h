a3fd7fec352e840c792bcb531ff8b01a8f47fd2b
sherrychen127 <sherrychen127@gmail.com>
2018-10-20 17:50:31 -0400

    Update jetson_i2c_test.h

--- jetson_i2c_test.h
+++ jetson_i2c_test.h
@@ -1,1 +1,17 @@
 
+#include <cstddef>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <cstdlib>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <time.h>
+
