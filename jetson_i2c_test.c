6c6f3dceaaf1ee931e2cfd45a1b42c2fb1712bc1
sherrychen127 <sherrychen127@gmail.com>
2018-10-20 18:19:50 -0400

    Update jetson_i2c_test.c

--- jetson_i2c_test.c
+++ jetson_i2c_test.c
@@ -1,4 +1,17 @@
-#include <jetson_i2c_test.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <cstdlib>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <time.h>
 
 int get_filedescriptor(){
 	char fileNameBuffer[32];
